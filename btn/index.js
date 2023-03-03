fb14d8522dce125d0c40d53dccfa51e856ebbae6
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-03-03 17:31:35 +0300

    Add files via upload

---
+++ btn/index.js
@@ -0,0 +1,19 @@
+"use script"
+
+let mainbtn = document.getElementById("main__btn")
+let mainTitle = document.querySelector(".main__left--title");
+let mainDesc = document.querySelector(".main__left--desc");
+let mainMore = document.querySelector(".main__more");
+
+let mainRight = document.querySelector(".main__right");
+
+let addText = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. In tempora
+placeat voluptate inventore tempore. Obcaecati ea corporis est optio
+totam!`
+
+
+mainbtn.addEventListener("click" , function(){
+  console.log("click");
+
+  main
+})
